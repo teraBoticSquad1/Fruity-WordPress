70747fa64b5d3e0664331fdd8737543bc1bfef28
Salman Farshe <salman.zihad@involve.ai>
2023-12-10 02:44:10 +0600

    Basic Task Completed

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+
+/** 
+ * Theme Functions goes here 
+ **/
+add_theme_support('title-tag');
+
+// calling all important files like css js jquery and bootstrap
+function fruity_register_important_files()
+{
+    wp_enqueue_style('fruity_style', get_stylesheet_uri());
+
+    // registering bootstrap
+    wp_register_style('fruity_bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), "4.0.0", "all");
+    wp_enqueue_style('fruity_bootstrap');
+
+    //google font
+    wp_enqueue_style('fruity_google_font', 'https://fonts.googleapis.com/css2?family=Belanosima&display=swap', false);
+
+
+    // registering js
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('fruity_bootstrap_js', get_template_directory_uri() . '/js/bootstrap.js', array(), "4.0.0", "true");
+    wp_enqueue_script('fruity_root_js', get_template_directory_uri() . '/js/root.js', array(),  "1.0.0", "true");
+}
+add_action("wp_enqueue_scripts", "fruity_register_important_files");
+
+// registering menu
+register_nav_menu('fruity_menu', __('Header Menu', 'fruity'));
--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?> class="no-js">
+
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <!-- calling wp head -->
+    <?php wp_head(); ?>
 </head>
-<body>
-    <php 
-        echo "hello"
-    ?>
+
+<body <?php body_class(); ?>>
+    <!-- calling wp foot -->
+    <?php wp_footer(); ?>
+    <div id="header_area" class="<?php echo get_theme_mod('fruity_menu_position'); ?>">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-3">
+                    <a href="">
+                        <img class="logo" src="<?php echo get_theme_mod('fruity_header_logo'); ?>" alt="" class="">
+                    </a>
+                </div>
+                <div class="col-md-9">
+                    <?php wp_nav_menu(array("theme_location" => "fruity_menu", "menu_id" => "nav")); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
