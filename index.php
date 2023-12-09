ac81142ea81bdbb396b17b02fdcdf466d83a3209
Salman Farshe <salman.zihad@involve.ai>
2023-12-10 02:52:07 +0600

    Basic Task Completed

--- index.php
+++ index.php
@@ -13,18 +13,9 @@
 <body <?php body_class(); ?>>
     <!-- calling wp foot -->
     <?php wp_footer(); ?>
-    <div id="header_area" class="<?php echo get_theme_mod('fruity_menu_position'); ?>">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-3">
-                    <a href="">
-                        <img class="logo" src="<?php echo get_theme_mod('fruity_header_logo'); ?>" alt="" class="">
-                    </a>
-                </div>
-                <div class="col-md-9">
-                    <?php wp_nav_menu(array("theme_location" => "fruity_menu", "menu_id" => "nav")); ?>
-                </div>
-            </div>
+    <div class="container">
+        <div class="display-2">
+            Fruity
         </div>
     </div>
 </body>
